8c9ff3e5a4c380b46295d3b4738efa8a52be810a
ty4r <ty4rsig@gmail.com>
2020-05-30 06:08:08 +0900

    test

--- test/test.js
+++ test/test.js
@@ -67,7 +67,12 @@ describe('/schedules', () => {
           const createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
             .expect(200)
             .end((err, res) => {
               if (err) return done(err);
